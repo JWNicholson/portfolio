398b784cd33726a64a32a8f0447b6a9cbfcb509f
JW Nicholson <john.nicholson812@gmail.com>
2020-08-16 15:32:33 -0400

    improved styling for StartStop button

--- src/components/Buttons/StartStop.js
+++ src/components/Buttons/StartStop.js
@@ -1,35 +1,27 @@
 import React, { useState } from 'react';
 
 const StartStop=()=>{
-   
     const [color, setColor] = useState('green')
     const [toggle, setToggle] = useState(false);
 
-  
-    
+    //onClick function to change button background:color and provide True or False to toggle button text
    const toggleBtn=()=> {
         setToggle(!toggle)
       if(toggle === false){
-          console.log('false')
+          //console.log('false')
           setColor('red')
-
       }else{
           setColor('green')
       }
    
        }
     
-
-
     return(
-        <div className="power-btn">
-           
-            <button 
+        <div className="power-btn-ctnr">
+            <button className="power-btn data-cy=pwrbtn"
              style={{background:color}}
-                onClick={toggleBtn }>
+                onClick={toggleBtn}>   
                 {toggle?'Stop': 'Start'}
-                
-             
             </button>
         </div>
     );
