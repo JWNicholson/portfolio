bf65fc05a1204d7857c25ee44b3f564371b25835
JW Nicholson <john.nicholson812@gmail.com>
2020-08-17 11:03:18 -0400

    working on Header with responsive navigation/hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-export default class Header extends Component {
-	render () {
-		return (
-			<div>
-				<header className="App-header">
-					<navbar>
-						<div className="logo-container">
-							<a href="/">
-								<h1 className="headerH1">John Nicholson</h1>
-								<p>
-									<small>Full Stack Developer</small>
-								</p>
-							</a>
-						</div>
-						<ul className="navlist">
-							<li className="navlist-item">
-								<a href="/">Home</a>
-							</li>
-							<li className="navlist-item">
-								<a href="/">Projects</a>
-							</li>
-							<li className="navlist-item">
-								<a href="/">Contact Me</a>
-							</li>
-						</ul>
-					</navbar>
-				</header>
-			</div>
-		);
-	}
+export default function Header () {
+	const [
+		isVisible,
+		setIsVisible
+	] = useState(true);
+	const [
+		isSmallScreen,
+		setIsSmallScreen
+	] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 700px)');
+		mediaQuery.addListener(handleMediaQueryChange);
+		handleMediaQueryChange(mediaQuery);
+
+		return () => {
+			mediaQuery.removeListener(handleMediaQueryChange);
+		};
+	}, []);
+
+	const handleMediaQueryChange = (mediaQuery) => {
+		if (mediaQuery.matches) {
+			setIsSmallScreen(true);
+		}
+		else {
+			setIsSmallScreen(false);
+		}
+	};
+
+	const toggleNav = () => {
+		setIsVisible(!isVisible);
+	};
+
+	return (
+		<header className="Header">
+			<h1 className="Logo">
+				John Nicholson <br />
+				<small>Full Stack Devleoper</small>
+			</h1>
+
+			<CSSTransition in={!isSmallScreen || isVisible} timeout={350} classNames="NavAnimation" unmountOnExit>
+				<nav className="Nav">
+					<a href="/">Home</a>
+					<a href="/">Articles</a>
+					<a href="/">About</a>
+				</nav>
+			</CSSTransition>
+			<button onClick={toggleNav} className="Burger">
+				<svg viewBox="0 0 100 80" width="40" height="40">
+					<rect width="100" height="20" />
+					<rect y="30" width="100" height="20" />
+					<rect y="60" width="100" height="20" />
+				</svg>
+			</button>
+		</header>
+	);
 }
