efa9df348821d553bde9dc8fe3bdd4ec7315f28d
JW Nicholson <john.nicholson812@gmail.com>
2020-08-19 10:37:16 -0400

    styling Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,17 +7,17 @@ export default class Nav extends Component {
     render() {
         return (
 <nav>
-    <ul class="menu">
+    <ul class="top-menu">
         <li class="logo"><a href="/">John Nicholson<br></br><small>Full Stack Devleoper</small></a></li>
-        <li class="nav-item"><a href="/">Home</a></li>
-        <li class="nav-item"><a href="/">Contact me</a></li>
-        <li class="nav-item"><a href="/">Blog</a></li>
-        <li class="nav-item has-submenu">
+        <li class="top-nav-item"><a href="/">Home</a></li>
+        <li class="top-nav-item"><a href="/">Contact me</a></li>
+        <li class="top-nav-item"><a href="/">Blog</a></li>
+        <li class="top-nav-item has-submenu">
             <a tabindex="0">Projects</a>
-            <ul class="nav-submenu">
-                <li class="nav-subitem"><a href="/">Conway's Game Of Life</a></li>
-                <li class="nav-subitem"><a href="/">NASA pic of the day</a></li>
-                <li class="nav-subitem"><a href="/">Prisoner Skills</a></li>
+            <ul class="top-nav-submenu">
+                <li class="top-nav-subitem"><a href="/">Conway's Game Of Life</a></li>
+                <li class="top-nav-subitem"><a href="/">NASA pic of the day</a></li>
+                <li class="top-nav-subitem"><a href="/">Prisoner Skills</a></li>
             </ul>
         </li>
  
