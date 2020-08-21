7690077bb979a5a300f3108393a0745e600711a6
JW Nicholson <john.nicholson812@gmail.com>
2020-08-21 09:20:11 -0400

    added Router routing

---
+++ src/Pages/gameOfLifePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GameOfLife extends Component {
+    render() {
+        return (
+            <div>
+                GAME OF LIFE
+            </div>
+        )
+    }
+}
