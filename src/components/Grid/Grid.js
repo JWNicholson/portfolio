2aa660a50b8a8d338cb98b2c4292b6d8606fb740
JW Nicholson <john.nicholson812@gmail.com>
2020-08-22 13:07:22 -0400

    initial states set

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, {useState,useRef, useEffect } from 'react';
 
 
 //Set the map coordinates pattern is allowed to travel
@@ -27,18 +27,51 @@ const newBoard = () => {
 }
 
 // Start with an empty Grid
+const gameRules = (g) => {
+  let newGrid = newBoard();
+  
+  //loop through neighboring cells by row & column
+  for (let i = 0; i < rows; i++){
+    for (let j = 0; j < cols; j++){
+      let neighbors = 0;
+      patternArea.forEach(([x,y]) => {
+        const blockX = i + x;
+        const blockY = j + y;
+        if (blockX >= 0 && blockX < rows && blockY >= 0 && blockY < cols) {
+          neighbors +=g[blockX][blockY];
+        }
+      });
+
+      // set rules for cell movement
+      if (neighbors < 2 || neighbors > 3) {
+        newGrid[i][j] = 0;
+      }else if (g[i][j] === 1 && (neighbors === 2 || neighbors ===3)) {
+        newGrid[i][j] = 1;
+      }else if (g[i][j] === 0 && neighbors === 3 ) {
+        newGrid[i][j] = 1;
+      }
+    }
+  }
+  return newGrid;
+};
 
 
-//Set rules for cell movement
 
 const Grid = () => {
 
-
 //First initial Grid state, which sets up double buffering
+const [frameOne, setFrameOne] = useState(() => {
+  return newBoard();
+});
+
+const frameOneRef = useRef(frameOne);
+frameOneRef.current = frameOne;
 
 
 //Second initial Grid state, to set up double buffering
-
+const [frameTwo, setFrameTwo] =  useState(() => {
+  return newBoard();
+});
 
 //Use state to check if game is running by setting initial state to False
 
@@ -79,7 +112,8 @@ export default Grid;
 /// Generate a new board (grid)
 
 /// Generate the newboard cells & life status
-//// Use game rules to check each live cell for neighbors - live or die accoridingly
+//// Use game rules to check each live cell for neighbors - live or die accoridingly.
+///// loop through neighboring cells by row & column
 
 /// Start/Stop button to start game
 /// Pause button to pause
@@ -88,6 +122,10 @@ export default Grid;
 /// Button to choose starting pattern or random
 /// Counter to display current generation number
 
+// -- use state - state of cells
+//-- Buttons should be individual components for re-use. Arrow functions should let them be used with differnet pages click handlers.?
 
+
+/// https://www.robinwieruch.de/react-state-array-add-update-remove
 
 
