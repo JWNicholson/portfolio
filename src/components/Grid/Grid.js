915e21fefabe4607b60d0d7262df8b647e14f40b
JW Nicholson <john.nicholson812@gmail.com>
2020-08-25 10:37:06 -0400

    working on 992px styles. need to finish and check 786px styles.Also combine merge grid.scss into gameoflife.scss

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,20 +1,41 @@
-import React, {useState,useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-import {
-  FaAngleDoubleDown,
-  FaAngleDoubleUp
-} from 'react-icons/fa';
+import { FaAngleDoubleDown, FaAngleDoubleUp } from 'react-icons/fa';
 
 //Set the map coordinates pattern is allowed to travel
 const patternArea = [
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, -1],
-  [0, 1],
-  [1, -1],
-  [1, 0],
-  [1, 1],
+	[
+		-1,
+		-1
+	],
+	[
+		-1,
+		0
+	],
+	[
+		-1,
+		1
+	],
+	[
+		0,
+		-1
+	],
+	[
+		0,
+		1
+	],
+	[
+		1,
+		-1
+	],
+	[
+		1,
+		0
+	],
+	[
+		1,
+		1
+	]
 ];
 
 // Grid size
@@ -23,335 +44,361 @@ let cols = 35;
 
 //build a Grid
 const newBoard = () => {
-  const clearedGrid = [];
-  for(let i = 0; i < rows; i++){
-    clearedGrid.push(Array.from(Array(cols), () => 0));
-  }
-  return clearedGrid;
-}
+	const clearedGrid = [];
+	for (let i = 0; i < rows; i++) {
+		clearedGrid.push(Array.from(Array(cols), () => 0));
+	}
+	return clearedGrid;
+};
 
 // Start with an empty Grid
 const gameRules = (g) => {
-  let newGrid = newBoard();
-  
-  //loop through neighboring cells by row & column
-  for (let i = 0; i < rows; i++){
-    for (let j = 0; j < cols; j++){
-      let neighbors = 0;
-      patternArea.forEach(([x,y]) => {
-        const blockX = i + x;
-        const blockY = j + y;
-        if (blockX >= 0 && blockX < rows && blockY >= 0 && blockY < cols) {
-          neighbors +=g[blockX][blockY];
-        }
-      });
+	let newGrid = newBoard();
 
-      // set rules for cell movement
-      if (neighbors < 2 || neighbors > 3) {
-        newGrid[i][j] = 0;
-      }else if (g[i][j] === 1 && (neighbors === 2 || neighbors ===3)) {
-        newGrid[i][j] = 1;
-      }else if (g[i][j] === 0 && neighbors === 3 ) {
-        newGrid[i][j] = 1;
-      }
-    }
-  }
-  return newGrid;
+	//loop through neighboring cells by row & column
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let neighbors = 0;
+			patternArea.forEach(([ x, y
+			]) => {
+				const blockX = i + x;
+				const blockY = j + y;
+				if (blockX >= 0 && blockX < rows && blockY >= 0 && blockY < cols) {
+					neighbors += g[blockX][blockY];
+				}
+			});
+
+			// set rules for cell movement
+			if (neighbors < 2 || neighbors > 3) {
+				newGrid[i][j] = 0;
+			}
+			else if (g[i][j] === 1 && (neighbors === 2 || neighbors === 3)) {
+				newGrid[i][j] = 1;
+			}
+			else if (g[i][j] === 0 && neighbors === 3) {
+				newGrid[i][j] = 1;
+			}
+		}
+	}
+	return newGrid;
 };
 
 const Grid = () => {
+	//First initial Grid state, which sets up double buffering
+	const [
+		frameOne,
+		setFrameOne
+	] = useState(() => {
+		return newBoard();
+	});
 
-//First initial Grid state, which sets up double buffering
-const [frameOne, setFrameOne] = useState(() => {
-  return newBoard();
-});
+	const frameOneRef = useRef(frameOne);
+	frameOneRef.current = frameOne;
 
-const frameOneRef = useRef(frameOne);
-frameOneRef.current = frameOne;
+	//Second initial Grid state, to set up double buffering
+	const [
+		frameTwo,
+		setFrameTwo
+	] = useState(() => {
+		return newBoard();
+	});
 
+	//Use state to check if game is running by setting initial state to False
+	const [
+		running,
+		setRunning
+	] = useState(false);
 
-//Second initial Grid state, to set up double buffering
-const [frameTwo, setFrameTwo] =  useState(() => {
-  return newBoard();
-});
+	//Check which grid is active
+	const [
+		activeFrame,
+		setActiveFrame
+	] = useState(1);
 
-//Use state to check if game is running by setting initial state to False
-const [running, setRunning] = useState(false);
+	//Generation Counter
+	const [
+		genCount,
+		setGenCount
+	] = useState(0);
 
-//Check which grid is active
-const [activeFrame, setActiveFrame] = useState(1)
+	//Set default speed
+	const [
+		speed,
+		setSpeed
+	] = useState(500);
 
-//Generation Counter
-const [genCount, setGenCount] = useState(0);
+	//Set speed reference
+	// useRef returns a mutable object whose .current property is initialized to the initial value passed as an arugument. The returned object persists fot the lifetime of the component
+	//useRef will give you the same ref object on every render.
+	const speedRef = useRef(speed);
+	speedRef.current = speed;
 
-//Set default speed
-const [speed, setSpeed] = useState(500);
+	// Double buffer --
+	//  when the active grid is 1, set frameOne's state in the gameRules function.
+	//Then set that into frameTwo.
+	//Else, if frameTwo is active set it in the gameRules
+	//Put that in frameOne's state. This gets it ready to be handed off
+	//This is also where the generation Counter is located
+	const nextGen = () => {
+		setGenCount(genCount + 1);
+		if (activeFrame === 1) {
+			setFrameTwo(gameRules(frameOne));
+			setActiveFrame(2);
+		}
+		else {
+			setFrameOne(gameRules(frameTwo));
+			setActiveFrame(1);
+		}
+	};
 
-//Set speed reference
-// useRef returns a mutable object whose .current property is initialized to the initial value passed as an arugument. The returned object persists fot the lifetime of the component
-//useRef will give you the same ref object on every render.
-const speedRef = useRef(speed);
-speedRef.current = speed;
+	const grid = activeFrame === 1 ? frameOne : frameTwo;
 
- // Double buffer --
-  //  when the active grid is 1, set frameOne's state in the gameRules function. 
-  //Then set that into frameTwo. 
-  //Else, if frameTwo is active set it in the gameRules
-  //Put that in frameOne's state. This gets it ready to be handed off
-  //This is also where the generation Counter is located
- const nextGen = () => {
-   setGenCount(genCount + 1);
-   if (activeFrame === 1) {
-     setFrameTwo(gameRules(frameOne));
-     setActiveFrame(2);
-   }else {
-     setFrameOne(gameRules(frameTwo));
-     setActiveFrame(1);
-   }
- };
+	//Game Logic
+	useEffect(
+		() => {
+			let gameEngine = null;
+			if (activeFrame && running) {
+				gameEngine = setInterval(
+					() => {
+						nextGen();
+					},
+					speedRef.current,
+					frameOneRef.current
+				);
+			}
+			else if (!running) {
+				clearInterval(gameEngine);
+				return;
+			}
+			return () => clearInterval(gameEngine);
+		},
+		[
+			activeFrame,
+			nextGen,
+			running
+		]
+	);
 
- const grid = activeFrame === 1 ? frameOne : frameTwo;
+	/** Button functions */
+	const [
+		color,
+		setColor
+	] = useState('green');
+	const [
+		toggle,
+		setToggle
+	] = useState(false);
+	const toggleBtn = () => {
+		setToggle(!toggle);
+		if (toggle === false) {
+			//console.log('false')
+			setColor('red');
+			console.log('runBtnHandler red');
+		}
+		else {
+			setColor('green');
+			console.log('runBtnHandler green');
+		}
+	};
 
-  //Game Logic
-  useEffect(() => {
-    let gameEngine = null;
-    if (activeFrame && running) {
-      gameEngine = setInterval(
-        () => {
-          nextGen();
-        },
-        speedRef.current,
-        frameOneRef.current
-      );
-    } else if (!running) {
-      clearInterval(gameEngine);
-      return;
-    }
-    return () => clearInterval(gameEngine);
-  }, [activeFrame,nextGen, running]);
+	const startStop = () => {
+		setRunning(!running);
+	};
 
-/** Button functions */
-const [color, setColor] = useState('green');
-const [toggle, setToggle] = useState(false);
-const toggleBtn =()=>{
-  setToggle(!toggle)
-  if(toggle === false){
-      //console.log('false')
-      setColor('red')
-      console.log("runBtnHandler red")
-  }else{
-      setColor('green')
-      console.log("runBtnHandler green")
-  }
-}
+	const clearGrid = () => {
+		console.log('clearGrid button');
+		if (running) {
+			setRunning(!running);
+		}
+		setFrameOne(newBoard());
+		setFrameTwo(newBoard());
+		setGenCount(0);
+	};
 
-const startStop = () => {
-  setRunning(!running)
-}
+	const beehive = () => {
+		console.log('beehive');
+		const newGrid = Array.from(grid);
+		newGrid[16][16] = 1;
+		newGrid[17][16] = 1;
+		newGrid[17][15] = 1;
+		newGrid[18][14] = 1;
+		newGrid[19][15] = 1;
+		newGrid[19][16] = 1;
+		newGrid[18][17] = 1;
 
-const clearGrid = () => {
-  console.log("clearGrid button")
-  if (running) {
-    setRunning(!running)
-  }
-  setFrameOne(newBoard())
-  setFrameTwo(newBoard())
-  setGenCount(0)
-}
+		if (activeFrame === 1) {
+			setFrameOne(newGrid);
+		}
+		else {
+			setFrameTwo(newGrid);
+		}
+	};
 
-const beehive = () => {
-  console.log("beehive")
-  const newGrid = Array.from(grid);
-  newGrid[16][16] = 1;
-  newGrid[17][16] = 1;
-  newGrid[17][15] = 1;
-  newGrid[18][14] = 1;
-  newGrid[19][15] = 1;
-  newGrid[19][16] = 1;
-  newGrid[18][17] = 1;
+	const spaceShip = () => {
+		console.log('spaceShip');
+		const newGrid = Array.from(grid);
+		newGrid[11][20] = 1;
+		newGrid[14][16] = 1;
+		newGrid[15][19] = 1;
+		newGrid[15][18] = 1;
+		newGrid[15][17] = 1;
+		newGrid[15][16] = 1;
+		newGrid[14][16] = 1;
+		newGrid[13][16] = 1;
+		newGrid[12][17] = 1;
+		newGrid[14][20] = 1;
+		if (activeFrame === 1) {
+			setFrameOne(newGrid);
+		}
+		else {
+			setFrameTwo(newGrid);
+		}
+	};
 
-  if (activeFrame === 1) {
-    setFrameOne(newGrid);
-  } else {
-    setFrameTwo(newGrid);
-  }
-}
+	const random = () => {
+		const randomGrid = [];
+		for (let i = 0; i < rows; i++) {
+			console.log('random button');
+			randomGrid.push(Array.from(Array(cols), () => (Math.random() > 0.5 ? 1 : 0)));
+		}
 
-const spaceShip = () => {
-  console.log("spaceShip")
-  const newGrid = Array.from(grid);
-  newGrid[11][20] = 1;
-  newGrid[14][16] = 1;
-  newGrid[15][19] = 1;
-  newGrid[15][18] = 1;
-  newGrid[15][17] = 1;
-  newGrid[15][16] = 1;
-  newGrid[14][16] = 1;
-  newGrid[13][16] = 1;
-  newGrid[12][17] = 1;
-  newGrid[14][20] = 1;
-  if (activeFrame === 1) {
-    setFrameOne(newGrid);
-  } else {
-    setFrameTwo(newGrid);
-  }
-}
+		if (activeFrame === 1) {
+			setFrameOne(randomGrid);
+		}
+		else {
+			setFrameTwo(randomGrid);
+		}
+	};
 
-const random = () => {
-  const randomGrid = [];
-  for (let i = 0; i < rows; i++) {
-    console.log("random button")
-    randomGrid.push(
-      Array.from(Array(cols), () => (Math.random() > 0.5 ? 1 : 0))
-    );
-  }
+	const slower = () => {
+		console.log('slower');
+		setSpeed(1000);
+	};
 
+	const faster = () => {
+		console.log('faster');
+		setSpeed(50);
+	};
 
-  if (activeFrame === 1) {
-    setFrameOne(randomGrid);
-  } else {
-    setFrameTwo(randomGrid);
-  }                    
-  }
+	const oneStep = () => {
+		console.log('oneStep');
+		nextGen();
+	};
 
-const slower = () => {
-  console.log("slower")
-  setSpeed(1000)
-}
+	/* click multi handlers */
+	const runBtnHandler = () => {
+		console.log('runBtnHandler');
+		startStop();
+		toggleBtn();
+	};
 
-const faster = () => {
-  console.log("faster")
-  setSpeed(50)
-}
+	/** end button functions */
 
-const oneStep = () => {
-  console.log("oneStep")
-  nextGen()
-}
+	return (
+		<div className="gol-grid-wrapper">
+			<div
+				className="gol-grid-ctnr"
+				style={{
+					display             : 'grid',
+					gridTemplateColumns : `repeat(${cols}, 18px)`
+				}}
+			>
+				{grid.map((row, i) =>
+					row.map((col, j) => (
+						<div
+							className={grid[i][j] ? 'grid-cells' : ''}
+							key={`${i}-${j}`}
+							onClick={() => {
+								if (running) {
+									return;
+								}
+								const newGrid = Array.from(grid);
+								newGrid[i][j] = grid[i][j] ? 0 : 1;
+								if (activeFrame === 1) {
+									setFrameOne(newGrid);
+								}
+								else {
+									setFrameTwo(newGrid);
+								}
+							}}
+							style={{
+								width: 18,
+								height: 18,
+								border: '1px solid lightgray',
+								background: 'ghostwhite',
+								margin: '0'
+							}}
+						/>
+					))
+				)}
+			</div>
 
-/* click multi handlers */
-const runBtnHandler = () =>{
-   console.log("runBtnHandler")
-   startStop()
-   toggleBtn()
-}
+			<div className="cntrl-panel">
+				<div className="counter-cntr">
+					<p className="count">Generation Count: {genCount}</p>
+				</div>
 
+				<div className="cntrl-btns-cntr">
+					<section className="power-clear-btn-group">
+						{/* start/stop */}
+            <h3>Controls</h3>
+						<button
+							className="power-btn"
+							data-cy="power-btn"
+							style={{ background: color }}
+							onClick={runBtnHandler}
+						>
+							{toggle ? 'Stop' : 'Start'}
+						</button>
 
-/** end button functions */
+						{/* clear grid */}
+						<button className="clear-btn" data-cy="clear-btn" onClick={clearGrid}>
+							Clear
+						</button>
+              
+						<hr />
+					</section>
+					<section className="shapes-btn-group">
+						<h3>Shapes</h3>
+						<button className="beeHive-btn" data-cy="beeHive-btn" onClick={beehive}>
+							Beehive
+						</button>
 
+						<button className="spaceShip-btn" data-cy="spaceShip-btn" onClick={spaceShip}>
+							Spaceship
+						</button>
 
-  return (
-<div className="gol-grid-wrapper">
+						<button
+							className="random-btn"
+							data-cy="random-btn"
+							onClick={random}>
+							Random
+						</button>
 
- 
+						<hr />
+					</section>
 
-      <div className="gol-grid-ctnr" 
-         style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${cols}, 18px)`,
-        }}
-         >
-        
-        {grid.map((row, i) =>
-            row.map((col, j) => (
-              <div
-                className={grid[i][j] ? "grid-cells" : ""}
-                key={`${i}-${j}`}
-                onClick={() => {
-                  if (running) {
-                    return;
-                  }
-                  const newGrid = Array.from(grid);
-                  newGrid[i][j] = grid[i][j] ? 0 : 1;
-                  if (activeFrame === 1) {
-                    setFrameOne(newGrid);
-                  } else {
-                    setFrameTwo(newGrid);
-                  }
-                }}
-                style={{width:18, height:18, border:"1px solid lightgray",background:"ghostwhite", margin:"0"}}
-              />
-            ))
-         )}
-      </div>
-       
-      <div className="cntrl-panel">
+					<section className="speed-btn-group">
+						<h3>Speed Controls</h3>
+						<button className="slower-btn" data-cy="slower-btn" onClick={slower}>
+							Slower <FaAngleDoubleDown />
+						</button>
 
-      <div className="counter-cntr">
-               <p className="count">Generation Count: {genCount}</p>   
-          </div> 
+						<button className="faster-btn" data-cy="faster-btn" onClick={faster}>
+							Faster <FaAngleDoubleUp />
+						</button>
 
-              <div className="cntrl-btns-cntr">
-            {/* start/stop */}
-              <button className="power-btn" 
-                      data-cy="power-btn"
-                      style={{background:color}}
-                      onClick={runBtnHandler}>   
-                      {toggle?'Stop': 'Start'}
-              </button>
-
-                {/* clear grid */}
-              <button className="clear-btn"
-                      data-cy="clear-btn"
-                      onClick={clearGrid}
-                      >Clear</button> 
-
-                <hr></hr>
-              <h3>Shapes</h3>
-              <button className="beeHive-btn"
-                      data-cy="beeHive-btn"
-                      onClick={beehive}
-                      >Beehive</button>
-
-              <button className="spaceShip-btn"
-                      data-cy="spaceShip-btn"
-                      onClick={spaceShip}
-                      >Spaceship</button>
-
-              <button className="random-btn" 
-                      data-cy="random-btn"
-                      onClick={random}
-                      // onClick={() => {
-                      //   const randomGrid = [];
-                      //   for (let i = 0; i < rows; i++) {
-                      //     randomGrid.push(
-                      //       Array.from(Array(cols), () => (Math.random() > 0.5 ? 1 : 0))
-                      //     );
-                      //   }
-                      //   if (activeFrame === 1) {
-                      //     setFrameOne(randomGrid);
-                      //   } else {
-                      //     setFrameTwo(randomGrid);
-                      //   }
-                      // }}
-                      >Random</button>
-
-                <hr></hr>
-
-                <h3>Speed Controls</h3>
-              <button className="slower-btn"
-                      data-cy="slower-btn"
-                      onClick={slower}
-                      >Slower <FaAngleDoubleDown/></button>
-
-              <button className="faster-btn"
-                      data-cy="faster-btn"
-                      onClick={faster}
-                      >Faster <FaAngleDoubleUp /></button>
-
-              <button className="oneStep-btn"
-                      data-cy="oneStep-btn"
-                      onClick={oneStep}>One Step</button>
-              </div>
-              </div>
-
-    </div>
-  )
-
-}
+						<button className="oneStep-btn" data-cy="oneStep-btn" onClick={oneStep}>
+							One Step
+						</button>
+					</section>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Grid;
-
 
 //******** Notes ***********/
 
@@ -375,9 +422,6 @@ export default Grid;
 // -- 1 is active, 0 is off
 //-- Buttons should be individual components for re-use. Arrow functions should let them be used with differnet pages click handlers.?
 
-
 /// Array.from() - https://www.geeksforgeeks.org/javascript-array-from-method-2/
 
 /// https://www.robinwieruch.de/react-state-array-add-update-remove
-
-
