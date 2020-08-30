99e7e373125bb26bce8b8da2cdae500fa57ccbff
JW Nicholson <john.nicholson812@gmail.com>
2020-08-30 19:48:14 -0400

    card functioning with weather dynamic weather icon. needs searchbar function and styliing for all

--- src/components/WeatherApp/WeatherCard.js
+++ src/components/WeatherApp/WeatherCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const WeatherCard = (props) => {
+    const icon_url = `http://openweathermap.org/img/wn/${props.weather_icon}@4x.png`
 	return (
 		<div className="weather-card-wrapper">
 			<div className="weather-card-header">
@@ -10,27 +11,69 @@ const WeatherCard = (props) => {
 			<div className="weather-searchBar">searchBar</div>
 
 			<div className="weather-conditions-table">
-				<h4>Current Conditions For {props.name}, {props.country}</h4>
+				<div className="weather-conditions-table-title">
+					<h4>
+						Current Conditions For {props.name}, {props.country}
+					</h4>
+				</div>
 
 				<div className="weather-table-data-wrapper">
-    <div className="weather-icon-ctnr">
-        <p>weather icon</p>
-        <p>"${props.weather_icon}"</p></div>
+					<div className="weather-icon-ctnr">
+						<p>weather icon</p>
+                        <img src={icon_url} alt="weather icon"/>
+						{/* <p>"${props.weather_icon}"</p> */}
+					</div>
 
 					<div className="weather-conditions-ctnr">
-						<h4>Current Weather Conditions</h4>
-						<p>Temp F&deg; - {props.temp}</p>
-						<p>Feels like - {props.feels}&deg;</p>
-                        <p>High - {props.high_temp}&deg;</p>
-                        <p>Low - {props.low_temp}&deg;</p>
-                        <p>Humidity - {props.humidity}%</p>
-                        {/* <p>Pressure - {props.pressure}</p> */}
-                        <p>Wind - {props.wind}mph</p>
-                        <p>Visibility - {props.visibility}ft</p>
+						<div className="weather-conditions-temp-ctnr">
+							<div className="cond-data-row">
+								<div className="cond-tag">Temp - </div>
+								<div className="cond-data">{Math.round(props.temp)}F&deg;</div>
+							</div>
+
+							<div className="cond-data-row">
+								<div className="cond-tag">Feels like - </div>
+								<div className="cond-data">{Math.round(props.feels)}F&deg;</div>
+							</div>
+
+							<div className="cond-data-row">
+								<div className="cond-tag">High - </div>
+								<div className="cond-data">{Math.round(props.high_temp)}F&deg;</div>
+							</div>
+
+							<div className="cond-data-row">
+								<div className="cond-tag">Low - </div>
+								<div className="cond-data">{Math.round(props.low_temp)}F&deg;</div>
+							</div>
+						</div>
+						{/* end weather-conditions-temp-ctnr */}
+
+						<div className="weather-conditions-atmos-ctnr">
+							<div className="cond-data-row">
+								<div className="cond-tag">Humidity - </div>
+								<div className="cond-data">{Math.round(props.humidity)}%</div>
+							</div>
+
+							<div className="cond-data-row">
+								<div className="cond-tag">Wind - </div>
+								<div className="cond-data">{Math.round(props.wind)}mph</div>
+							</div>
+
+							<div className="cond-data-row">
+								<div className="cond-tag">Visibility - </div>
+								<div className="cond-data">{Math.round(props.visibility)}ft</div>
+							</div>
+						</div>
+						{/* end weather-conditions-atmos-ctnr */}
 					</div>
+					{/* end weather-conditions-ctnr */}
 				</div>
+				{/* end-weather-table-data-wrapper */}
 			</div>
+
+			{/* end weather-condition-table */}
 		</div>
+		// end weather-card-wrapper
 	);
 };
 
--- src/components/WeatherApp/WeatherGrid.js
+++ src/components/WeatherApp/WeatherGrid.js
@@ -1,85 +1,84 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import WeatherCard from '../WeatherApp/WeatherCard';
 
 import axios from 'axios';
 
-const WTHR_API_KEY=process.env.REACT_APP_WTHR_KEY;
-const WTHR_API_URL=process.env.REACT_APP_WTHR_URL;
-const IMP = "imperial";
-const MET = "metric";
+const WTHR_API_KEY = process.env.REACT_APP_WTHR_KEY;
+const WTHR_API_URL = process.env.REACT_APP_WTHR_URL;
+const IMP = 'imperial';
+const MET = 'metric';
 
+export default function WeatherGrid () {
+	//set initail weather state as an empty array,
+	//because this API sends back an array
+	const [
+		weatherData,
+		setWeatherData
+	] = useState({});
+	const [
+		currMainData,
+		setCurrMainData
+	] = useState({});
+	const [
+		owSys,
+		setOwSys
+	] = useState({});
+	const [
+		weatherObj,
+		setWeatherObj
+	] = useState({});
+	const [
+		wind,
+		setWind
+	] = useState({});
 
-export default function WeatherGrid() {
-    //set initail weather state as an empty array,
-    //because this API sends back an array
-    const [weatherData, setWeatherData] = useState({})
-    const [currMainData,setCurrMainData] = useState({})
-    const [owSys,setOwSys] = useState({})
-    const [weatherObj,setWeatherObj] = useState({})
-    const [wind,setWind] = useState({})
+	//let city = `q=${city}`
 
-    //let city = `q=${city}`
+	useEffect(() => {
+		axios
+			.get(`${WTHR_API_URL}q=Louisville&units=${IMP}&appid=${WTHR_API_KEY}`)
+			.then((response) => {
+				//console.log(response.data);
+				setWeatherData(response.data);
+				setCurrMainData(response.data.main);
+				setOwSys(response.data.sys);
+				setWeatherObj(response.data.weather[0]);
+				setWind(response.data.wind);
+			})
+			.catch((error) => {
+				console.log('weather .get error: ', error);
+			});
+	}, []);
 
-    useEffect(() => {
-        
-        axios
-            .get(`${WTHR_API_URL}q=Louisville&units=${IMP}&appid=${WTHR_API_KEY}`)
-            .then((response) => {
-             console.log(response.data)
-                setWeatherData(response.data)
-                setCurrMainData(response.data.main)
-                setOwSys(response.data.sys)
-                setWeatherObj(response.data.weather[0])
-                setWind(response.data.wind)
-                
-            })
-            .catch((error) => {
-                console.log('weather .get error: ', error);
-            })
-    }, [])
+	console.log(weatherObj.icon);
 
-    //console.log(wind.speed);
-
-    return (
-      
-     
-      <WeatherCard
-        //use returned data here
-        key={weatherData.id}
-        //city name -temporary until user input is functioning
-        name={weatherData.name}
-
-       //Visibility
-       visibility={weatherData.visibility}
-
-        //country={weatherData.sys.country}
-        country={owSys.country}
-
-        //weather icon
-        weather_icon={weatherObj.icon}
-
-        //current temperature
-        temp={currMainData.temp}
-
-        //feels like
-        feels={currMainData.feels_like}
-
-        //High
-        high_temp={currMainData.temp_max}
-
-        //Low
-        low_temp={currMainData.temp_min}
-
-        //Pressure
-        pressure={currMainData.pressure}
-
-        //Humidity
-        humidity={currMainData.humidity}
-
-        //wind
-        wind={wind.speed}
-       
-      />
-    )
+	return (
+		<WeatherCard
+			//use returned data here
+			key={weatherData.id}
+			//city name -temporary until user input is functioning
+			name={weatherData.name}
+			//Visibility
+			visibility={weatherData.visibility}
+			//country={weatherData.sys.country}
+			country={owSys.country}
+			//weather icon
+			weather_icon={weatherObj.icon}
+			//current temperature
+			temp={currMainData.temp}
+			//feels like
+			feels={currMainData.feels_like}
+			//High
+			high_temp={currMainData.temp_max}
+			//Low
+			low_temp={currMainData.temp_min}
+			//Pressure
+			pressure={currMainData.pressure}
+			//Humidity
+			humidity={currMainData.humidity}
+			//wind
+			wind={wind.speed}
+		/>
+	);
 }
